f920223b0c46dee4c19cce55c3197771c5538448
John Atkins <johnatkins1999@yahoo.com>
2017-07-30 11:16:23 -1000

    colours

--- routes/trees.js
+++ routes/trees.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Handlebars = require('handlebars');
+
 router.get('/user/:id', loadUserPage);
 
 // router.get('/', getProducts);
@@ -18,8 +20,9 @@ router.get('/user/:id', loadUserPage);
 function loadUserPage(req, res){
   let id = Number(req.params.id);
   let rels = { relations: getRelations(id) };
-  console.log(rels);
-  res.render('mainList', rels);
+  let htmlString = new Handlebars.SafeString(createRelationsHTML(rels));
+  let locals = { ulstring : htmlString } ;
+  res.render('mainList', locals);
 }
 
 function getName(thisID){
@@ -90,6 +93,199 @@ function createRelObj(item, rel){
     url: item.photo_url
   };
 }
+
+function createRelationsHTML(relObj){
+
+  let relArray = relObj.relations;
+  let html = `<ul id="org" style="display:none">`;
+
+
+  let father = relArray.filter(rel => {
+    if(rel.relation === 'father'){
+      return rel;
+    }
+  });
+  //html += createListItem(father);
+
+  // let mother = relArray.filter(rel => {
+  //   if(rel.relation === 'mother'){
+  //     return rel;
+  //   }
+  // });
+  html += createMainParentItem(father);
+
+  let self = relArray.filter(rel => {
+    if(rel.relation === 'self' || rel.relation === 'wife' || rel.relation === 'husband' || rel.relation === 'daughter' || rel.relation === 'son' || rel.relation === 'extended family'){
+      return rel;
+    }
+  });
+
+  html += createSelfItem(self);
+
+  let siblings = relArray.filter(rel => {
+    if(rel.relation === 'brother' || rel.relation === 'sister'){
+      return rel;
+    }
+  });
+
+  html += createListItem(siblings) + '</li></ul></ul>';
+
+  return html;
+}
+
+function createSelfItem(relObj){
+
+  console.log(relObj);
+
+  let myself = relObj.filter(rel => {
+    if(rel.relation === 'self'){
+      return rel;
+    }
+  });
+
+  let children = relObj.filter(rel => {
+    if(rel.relation === 'son' || rel.relation === 'daughter' || rel.relation === 'extended family' ){
+      return rel;
+    }
+  });
+
+  // let extFam = relObj.filter(rel => {
+  //   if(rel.relation === 'extended family'){
+  //     return rel;
+  //   }
+  // });
+
+  let spouse = relObj.filter(rel => {
+    if(rel.relation === 'wife' || rel.relation === 'husband'){
+      return rel;
+    }
+  });
+
+  let html = `<ul><li class = "person">
+        <div class="person-outer ${myself[0].relation}">
+          <div class="person-top"></div>
+          <div class="person-middle">
+            <div class="person-left"></div>
+            <div class="person-photo">
+             <img class="person-img" src="/images/${myself[0].url}" alt="${myself[0].name} image">
+              <p class="personName">${myself[0].name}</p>
+              <p class="relationship">${myself[0].relation}</p>
+            </div>
+            <div class="person-right"></div>
+          </div>
+          <div class="person-bottom"></div>
+        </div>`;
+    if (children.length > 0) {
+          html += '<ul>';
+          for (var j = 0; j < children.length; j++) {
+              html += `<li class = "person">
+                  <div class="person-outer ${children[j].relation}">
+                    <div class="person-top"></div>
+                    <div class="person-middle">
+                      <div class="person-left"></div>
+                      <div class="person-photo">
+                       <img class="person-img" src="/images/${children[j].url}" alt="${children[j].name} image">
+                        <p class="personName">${children[j].name}</p>
+                        <p class="relationship">${children[j].relation}</p>
+                      </div>
+                      <div class="person-right"></div>
+                    </div>
+                    <div class="person-bottom"></div>
+                  </div>
+                  </li>`;
+          }
+          html += '</ul>';
+     }
+
+   // if (extFam.length > 0) {
+   //    html += '<ul>';
+   //    for (var k = 0; k < extFam.length; k++) {
+   //          html += `<li class = "person">
+   //              <div class="person-outer ${extFam[k].relation}">
+   //                <div class="person-top"></div>
+   //                <div class="person-middle">
+   //                  <div class="person-left"></div>
+   //                  <div class="person-photo">
+   //                   <img class="person-img" src="/images/${extFam[k].url}" alt="${extFam[k].name} image">
+   //                    <p class="personName">${extFam[k].name}</p>
+   //                    <p class="relationship">${extFam[k].relation}</p>
+   //                  </div>
+   //                  <div class="person-right"></div>
+   //                </div>
+   //                <div class="person-bottom"></div>
+   //              </div>`;
+   //          }
+   //      html += '</ul>';
+   //    }
+    html += '</li>';
+
+    if (spouse.length > 0){
+          for (var l = 0; l < spouse.length; l++) {
+              html += `<li class = "person">
+                  <div class="person-outer ${spouse[l].relation}">
+                    <div class="person-top"></div>
+                    <div class="person-middle">
+                      <div class="person-left"></div>
+                      <div class="person-photo">
+                       <img class="person-img" src="/images/${spouse[l].url}" alt="${spouse[l].name} image">
+                        <p class="personName">${spouse[l].name}</p>
+                        <p class="relationship">${spouse[l].relation}</p>
+                      </div>
+                      <div class="person-right"></div>
+                    </div>
+                    <div class="person-bottom"></div>
+                  </div>
+                  </li>`;
+            }
+      return html += '</ul>';
+    }
+}
+
+function createMainParentItem(relObj){
+  let html = '';
+
+  for (var i = 0; i < relObj.length; i++) {
+    html += `<li class = "person">
+      <div class="person-outer ${relObj[i].relation}">
+        <div class="person-top"></div>
+        <div class="person-middle">
+          <div class="person-left"></div>
+          <div class="person-photo">
+           <img class="person-img" src="/images/${relObj[i].url}" alt="${relObj.name} image">
+            <p class="personName">${relObj[i].name}</p>
+            <p class="relationship">${relObj[i].relation}</p>
+          </div>
+          <div class="person-right"></div>
+        </div>
+        <div class="person-bottom"></div>
+      </div>`;
+  }
+  return html;
+}
+
+function createListItem(relObj){
+  let html = '';
+
+  for (var i = 0; i < relObj.length; i++) {
+    html += `<li class = "person">
+      <div class="person-outer ${relObj[i].relation}">
+        <div class="person-top"></div>
+        <div class="person-middle">
+          <div class="person-left"></div>
+          <div class="person-photo">
+           <img class="person-img" src="/images/${relObj[i].url}" alt="${relObj.name} image">
+            <p class="personName">${relObj[i].name}</p>
+            <p class="relationship">${relObj[i].relation}</p>
+          </div>
+          <div class="person-right"></div>
+        </div>
+        <div class="person-bottom"></div>
+      </div>
+      </li>`;
+  }
+  return html;
+}
+
 
 let familyTree = { "family_members" :
   [
@@ -290,7 +486,7 @@ let familyTree = { "family_members" :
 
       {
           "id" : 18,
-          "fname" : "Hodor (Wylis)",
+          "fname" : "Hodor",
           "lname" : "",
           "gender": "male",
           "father_id" : 0,
@@ -328,8 +524,8 @@ let familyTree = { "family_members" :
 
 };
 
-(function(){
-  console.log(getRelations(5));
-})();
+// (function(){
+//   console.log(getRelations(5));
+// })();
 
 module.exports = router;
